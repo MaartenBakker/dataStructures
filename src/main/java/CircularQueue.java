6582b2077aff0a0f14bd5291ca551e3bb4ccd2d5
Maarten Bakker <maartenbakker@gmail.com>
2021-03-06 20:04:11 +0100

    Circular Queue implementation

---
+++ src/main/java/CircularQueue.java
@@ -0,0 +1,65 @@
+class MyCircularQueue {
+    private int head;
+    private int tail;
+    private final int[] array;
+
+    public MyCircularQueue(int k) {
+        array = new int[k];
+        reset();
+    }
+
+    public boolean enQueue(int value) {
+        if (isFull()) {
+            return false;
+        }
+        if (isEmpty()) {
+            head = 0;
+            tail = 0;
+        }
+        array[tail] = value;
+        tail++;
+        if (tail == array.length) {
+            tail = 0;
+        }
+        return true;
+    }
+
+    public boolean deQueue() {
+        if (!isEmpty()) {
+            head++;
+            if (head == array.length) {
+                head = 0;
+            }
+            if (head == tail) {
+                reset();
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public int Front() {
+        return isEmpty() ? -1 : array[head];
+    }
+
+    public int Rear() {
+        int rear = tail - 1;
+        if (rear < 0) {
+            rear = array.length - 1;
+        }
+        return isEmpty() ? -1 : array[rear];
+    }
+
+    public boolean isEmpty() {
+        return (head == -1 && tail == -1);
+    }
+
+    public boolean isFull() {
+        return (!isEmpty() && head == tail);
+    }
+
+    private void reset() {
+        head = -1;
+        tail = -1;
+    }
+}
